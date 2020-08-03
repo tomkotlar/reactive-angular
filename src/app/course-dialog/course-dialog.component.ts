7c07f465b228f03494aa6a489448ac9f72f093ea
tomkotlar <t.kotlar@yahoo.com>
2020-08-03 18:32:48 +0100

    Update -Injecting saveCourse func

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -5,6 +5,7 @@ import {FormBuilder, Validators, FormGroup} from "@angular/forms";
 import * as moment from 'moment';
 import {catchError} from 'rxjs/operators';
 import {throwError} from 'rxjs';
+import { CoursesService } from 'app/services/courses.service';
 
 @Component({
     selector: 'course-dialog',
@@ -20,7 +21,9 @@ export class CourseDialogComponent implements AfterViewInit {
     constructor(
         private fb: FormBuilder,
         private dialogRef: MatDialogRef<CourseDialogComponent>,
-        @Inject(MAT_DIALOG_DATA) course:Course) {
+        @Inject(MAT_DIALOG_DATA) course:Course,
+        private coursesService: CoursesService
+        ) {
 
         this.course = course;
 
@@ -40,7 +43,9 @@ export class CourseDialogComponent implements AfterViewInit {
     save() {
 
       const changes = this.form.value;
-
+    this.coursesService.saveCourse(this.course.id, changes).subscribe(
+        (val) => this.dialogRef.close(val)
+    )
     }
 
     close() {
