8569b44d27132aded77cd67abfb45d523aa4dba8
tomkotlar <t.kotlar@yahoo.com>
2020-08-03 18:31:35 +0100

    Example in Reactive Style (Stateless Application)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -23,16 +23,19 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
 
-   const courses$ = this.coursesService.loadAllCourses().pipe(
-     map(courses => courses.sort(sortCoursesBySeqNo))
-   )
-
-   this.beginnerCourses$ = courses$.pipe(map(courses => courses.filter(course => course.category == "BEGINNER")))
-   this.advancedCourses$ = courses$.pipe(map(courses => courses.filter(course => course.category == "ADVANCED")))
+  this.reloadCourses()
 
   }
 
   
+  reloadCourses() {
+    const courses$ = this.coursesService.loadAllCourses().pipe(
+      map(courses => courses.sort(sortCoursesBySeqNo))
+    )
+ 
+    this.beginnerCourses$ = courses$.pipe(map(courses => courses.filter(course => course.category == "BEGINNER")))
+    this.advancedCourses$ = courses$.pipe(map(courses => courses.filter(course => course.category == "ADVANCED")))
+  }
 
 }
 
