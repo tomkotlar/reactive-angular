93c57401257f088bafb5e4947d4de9865d38b428
tomkotlar <t.kotlar@yahoo.com>
2020-08-03 17:51:57 +0100

    Update- removing display logic to presnt. compone

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,8 +3,6 @@ import {Course, sortCoursesBySeqNo} from '../model/course';
 import {interval, noop, Observable, of, throwError, timer} from 'rxjs';
 import {catchError, delay, delayWhen, filter, finalize, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
 
-import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
-import {CourseDialogComponent} from '../course-dialog/course-dialog.component';
 import { CoursesService } from "../services/courses.service";
 
 @Component({
@@ -19,7 +17,7 @@ export class HomeComponent implements OnInit {
   advancedCourses$: Observable<Course[]>;
 
 
-  constructor(private coursesService: CoursesService, private dialog: MatDialog) {
+  constructor(private coursesService: CoursesService) {
 
   }
 
@@ -34,19 +32,7 @@ export class HomeComponent implements OnInit {
 
   }
 
-  editCourse(course: Course) {
-
-    const dialogConfig = new MatDialogConfig();
-
-    dialogConfig.disableClose = true;
-    dialogConfig.autoFocus = true;
-    dialogConfig.width = "400px";
-
-    dialogConfig.data = course;
-
-    const dialogRef = this.dialog.open(CourseDialogComponent, dialogConfig);
-
-  }
+  
 
 }
 
