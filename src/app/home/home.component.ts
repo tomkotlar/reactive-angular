3c6418536c20c9a23d5386f66ae7ac9a5c6d9fc6
tomkotlar <t.kotlar@yahoo.com>
2020-09-02 12:28:39 +0100

    Refactor(home.ts): Using showLoader method obser.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -30,15 +30,14 @@ export class HomeComponent implements OnInit {
 
   
   reloadCourses() {
-    this.loadingService.loadingOn()
-
     const courses$ = this.coursesService.loadAllCourses().pipe(
-      map(courses => courses.sort(sortCoursesBySeqNo)),
-      finalize(() => this.loadingService.loadingOff() )
+      map(courses => courses.sort(sortCoursesBySeqNo))
     )
+
+    const loadCourses$ = this.loadingService.showLoaderUntilCompleted(courses$)
  
-    this.beginnerCourses$ = courses$.pipe(map(courses => courses.filter(course => course.category == "BEGINNER")))
-    this.advancedCourses$ = courses$.pipe(map(courses => courses.filter(course => course.category == "ADVANCED")))
+    this.beginnerCourses$ = loadCourses$.pipe(map(courses => courses.filter(course => course.category == "BEGINNER")))
+    this.advancedCourses$ = loadCourses$.pipe(map(courses => courses.filter(course => course.category == "ADVANCED")))
   }
 
 }
