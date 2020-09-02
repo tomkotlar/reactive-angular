f9b5646dae4150971b974e21b491587e25970cc7
tomkotlar <t.kotlar@yahoo.com>
2020-09-02 12:05:31 +0100

    Feat(loading.servive): loadingSubject/methods

--- src/app/loading/loading.service.ts
+++ src/app/loading/loading.service.ts
@@ -5,6 +5,20 @@ import {concatMap, finalize, tap} from 'rxjs/operators';
 
 @Injectable()
 export class LoadingService {
+    private loadingSubject = new BehaviorSubject<boolean>(false)
 
-    
+    loading$: Observable<boolean> = this.loadingSubject.asObservable()
+
+    showLoaderUntilCompleted<T>(obs$: Observable<T>): Observable<T> {
+        return undefined
+    }
+
+    loadingOn() {
+       this.loadingSubject.next(true)
+
+    }
+
+    loadingOff() {
+        this.loadingSubject.next(false)
+    }
  }
