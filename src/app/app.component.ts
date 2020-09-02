c67a7aa5c73a6a0d1e232264ea2ef40582ab9990
tomkotlar <t.kotlar@yahoo.com>
2020-09-02 15:00:57 +0100

    Feat(app.ts): Injectig the MessageServie

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LoadingService } from './loading/loading.service';
+import { MessageService } from "./messages/messages.service";
 
 
 
@@ -8,7 +9,7 @@ import { LoadingService } from './loading/loading.service';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [ LoadingService]
+  providers: [ LoadingService, MessageService]
 })
 export class AppComponent implements  OnInit {
 
