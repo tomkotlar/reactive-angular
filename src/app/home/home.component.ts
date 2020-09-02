3ccef364a62deec55a93c965470fad8f044bc102
tomkotlar <t.kotlar@yahoo.com>
2020-09-02 12:06:53 +0100

    Faet(home.ts): Using LoadingService methods

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -30,8 +30,11 @@ export class HomeComponent implements OnInit {
 
   
   reloadCourses() {
+    this.loadingService.loadingOn()
+
     const courses$ = this.coursesService.loadAllCourses().pipe(
-      map(courses => courses.sort(sortCoursesBySeqNo))
+      map(courses => courses.sort(sortCoursesBySeqNo)),
+      finalize(() => this.loadingService.loadingOff() )
     )
  
     this.beginnerCourses$ = courses$.pipe(map(courses => courses.filter(course => course.category == "BEGINNER")))
