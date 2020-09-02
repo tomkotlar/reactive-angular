82ce75e53381b8e92c8ebeb66fd5b4e539b6e9e0
tomkotlar <t.kotlar@yahoo.com>
2020-09-02 13:16:55 +0100

    Refactor(course-dialog.ts): LoadingService seprate

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -11,7 +11,8 @@ import { LoadingService } from "../loading/loading.service";
 @Component({
     selector: 'course-dialog',
     templateUrl: './course-dialog.component.html',
-    styleUrls: ['./course-dialog.component.css']
+    styleUrls: ['./course-dialog.component.css'], 
+    providers: [ LoadingService ]
 })
 export class CourseDialogComponent implements AfterViewInit {
 
@@ -35,19 +36,21 @@ export class CourseDialogComponent implements AfterViewInit {
             releasedAt: [moment(), Validators.required],
             longDescription: [course.longDescription,Validators.required]
         });
-
     }
+
 
     ngAfterViewInit() {
 
     }
 
     save() {
-
       const changes = this.form.value;
-    this.coursesService.saveCourse(this.course.id, changes).subscribe(
-        (val) => this.dialogRef.close(val)
-    )
+      const saveCourse$ =  this.coursesService.saveCourse(this.course.id, changes);
+
+      this.loadingService.showLoaderUntilCompleted(saveCourse$)
+            .subscribe(
+                (val) => this.dialogRef.close(val)
+            )
     }
 
     close() {
