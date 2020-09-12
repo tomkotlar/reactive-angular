d7b1d1607fb2d81cf57230934d5baa7ccd32a094
tomkotlar <t.kotlar@yahoo.com>
2020-09-12 11:06:08 +0100

    Refactor(home.ts): to CourseStore state manag.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,8 +2,9 @@ import {Component, OnInit} from '@angular/core';
 import {Course, sortCoursesBySeqNo} from '../model/course';
 import {interval, noop, Observable, of, throwError, timer} from 'rxjs';
 import {catchError, delay, delayWhen, filter, finalize, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
+import { CoursesStore } from "../services/courses.store";
 
-import { CoursesService } from "../services/courses.service";
+// import { CoursesService } from "../services/courses.service";
 import { LoadingService } from 'app/loading/loading.service';
 import { MessageService } from 'app/messages/messages.service';
 
@@ -19,7 +20,12 @@ export class HomeComponent implements OnInit {
   advancedCourses$: Observable<Course[]>;
 
 
-  constructor(private coursesService: CoursesService, private loadingService: LoadingService, private messageService: MessageService) {
+  constructor(
+              // private coursesService: CoursesService,
+              private loadingService: LoadingService, 
+              private messageService: MessageService,
+              private courseStore: CoursesStore
+              ) {
 
   }
 
@@ -31,20 +37,24 @@ export class HomeComponent implements OnInit {
 
   
   reloadCourses() {
-    const courses$ = this.coursesService.loadAllCourses().pipe(
-      map(courses => courses.sort(sortCoursesBySeqNo)),
-      catchError(err => {
-        const errorMessage = 'Could Not Load Courses'
-        this.messageService.showErrors(errorMessage)
-        console.log(errorMessage)
-        return throwError(err)
-      })
-    )
+    // const courses$ = this.coursesService.loadAllCourses().pipe(
+    //   map(courses => courses.sort(sortCoursesBySeqNo)),
+    //   catchError(err => {
+    //     const errorMessage = 'Could Not Load Courses'
+    //     this.messageService.showErrors(errorMessage)
+    //     console.log(errorMessage)
+    //     return throwError(err)
+    //   })
+    // )
 
-    const loadCourses$ = this.loadingService.showLoaderUntilCompleted(courses$)
- 
-    this.beginnerCourses$ = loadCourses$.pipe(map(courses => courses.filter(course => course.category == "BEGINNER")))
-    this.advancedCourses$ = loadCourses$.pipe(map(courses => courses.filter(course => course.category == "ADVANCED")))
+    // const loadCourses$ = this.loadingService.showLoaderUntilCompleted(courses$)
+    // this.beginnerCourses$ = loadCourses$.pipe(map(courses => courses.filter(course => course.category == "BEGINNER")))
+    // this.advancedCourses$ = loadCourses$.pipe(map(courses => courses.filter(course => course.category == "ADVANCED")))
+
+    this.beginnerCourses$ = this.courseStore.filterByCategory("BEGINNER")
+    this.advancedCourses$ = this.courseStore.filterByCategory("ADVANCED")
+
+
   }
 
 }
