f3d3074eaf09aae55a04acac171c49bac0c95f23
tomkotlar <t.kotlar@yahoo.com>
2020-09-12 11:45:34 +0100

    Refactor(course-dialog.ts): CoursesStore state

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -3,11 +3,12 @@ import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
 import {Course} from "../model/course";
 import {FormBuilder, Validators, FormGroup} from "@angular/forms";
 import * as moment from 'moment';
-import {catchError} from 'rxjs/operators';
-import {throwError} from 'rxjs';
-import { CoursesService } from 'app/services/courses.service';
+// import {catchError} from 'rxjs/operators';
+// import {throwError} from 'rxjs';
+// import { CoursesService } from 'app/services/courses.service';
 import { LoadingService } from "../loading/loading.service";
 import { MessageService } from "../messages/messages.service";
+import { CoursesStore } from 'app/services/courses.store';
 
 @Component({
     selector: 'course-dialog',
@@ -25,9 +26,10 @@ export class CourseDialogComponent implements AfterViewInit {
         private fb: FormBuilder,
         private dialogRef: MatDialogRef<CourseDialogComponent>,
         @Inject(MAT_DIALOG_DATA) course:Course,
-        private coursesService: CoursesService, 
-        private loadingService: LoadingService,
+        // private coursesService: CoursesService, 
+        // private loadingService: LoadingService,
         private messagesSerivce: MessageService,
+        private coursesStore: CoursesStore
         ) {
 
         this.course = course;
@@ -47,18 +49,21 @@ export class CourseDialogComponent implements AfterViewInit {
 
     save() {
       const changes = this.form.value;
-      const saveCourse$ =  this.coursesService.saveCourse(this.course.id, changes)
-      .pipe(catchError(err => {
-          const message = "Could not save the Course"
-          console.log(err, message)
-          this.messagesSerivce.showErrors(message)
-          return throwError(err)
-      }))
+    //   const saveCourse$ =  this.coursesService.saveCourse(this.course.id, changes)
+    //   .pipe(catchError(err => {
+    //       const message = "Could not save the Course"
+    //       console.log(err, message)
+    //       this.messagesSerivce.showErrors(message)
+    //       return throwError(err)
+    //   }))
 
-      this.loadingService.showLoaderUntilCompleted(saveCourse$)
-            .subscribe(
-                (val) => this.dialogRef.close(val)
-            )
+      this.coursesStore.saveCourse(this.course.id, changes).subscribe()
+      this.dialogRef.close(changes)
+
+    //   this.loadingService.showLoaderUntilCompleted(saveCourse$)
+    //         .subscribe(
+    //             (val) => this.dialogRef.close(val)
+    //         )
     }
 
     close() {
